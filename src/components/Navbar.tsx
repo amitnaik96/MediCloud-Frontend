16d46f19fddd3e59caac8803845a41f53650074f
Amit Naik <amitnaikkwr@gmail.com>
2025-01-01 02:07:08 +0530

    medicloud nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,9 @@ const Navbar = () => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
                     </div>
-                    <div className="flex flex-col justify-center text-xl font-bold text-sky-900">Medicloud.</div>
+                    <Link to={'/'}>
+                        <div className="flex flex-col justify-center text-xl font-bold text-sky-900">Medicloud.</div>
+                    </Link>
                 </div>
                 <div className="flex mt-2 mr-3">
                     <Link to={'/signin'} className="mr-2">
