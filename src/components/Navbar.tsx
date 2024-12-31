1e797af1a1df02751e0746b0d181c63c0532466d
Amit Naik <amitnaikkwr@gmail.com>
2025-01-01 02:11:19 +0530

    navbar mobile fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = () => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
                     </div>
-                    <Link to={'/'}>
-                        <div className="flex flex-col justify-center text-xl font-bold text-sky-900">Medicloud.</div>
+                    <Link to={'/'} className="flex flex-col justify-center text-xl font-bold text-sky-900">
+                        <div >Medicloud.</div>
                     </Link>
                 </div>
                 <div className="flex mt-2 mr-3">
